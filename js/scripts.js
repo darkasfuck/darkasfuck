b6b3354feb982c65e6921df1a96d3b9f5659e711
darkasfuck <auriarad@gmail.com>
2024-03-01 17:00:41 +0200

    b

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+/*!
+* Start Bootstrap - Blog Home v5.0.9 (https://startbootstrap.com/template/blog-home)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+class joke {
+    constructor(title, body) {
+      this.title = title;
+      this.body = body;
+    }
+  }
+  fetch('C:\Users\auria\OneDrive\Desktop\website\בית\assets')
+  .then(response => response.text())
+  .then(text => {
+     print (text);
+     console.log(text)
+  })
+
+var jokeslist = new Array();
+while (true){
+    jk = new joke ("a","a");
+    jokeslist.push(jk);
+}
