3ddb0c441b3d9368fff6172ac748a60fdea83cd8
darkasfuck <auriarad@gmail.com>
2024-03-15 16:42:33 +0200

    a

--- js/DisplayLoop.js
+++ js/DisplayLoop.js
@@ -1,27 +1,24 @@
-/*
-const main = document.querySelector('main');
+//const main = document.querySelector('main');
 
+
+let Ljokes = [];
+fetch('https://raw.githubusercontent.com/darkasfuck/darkasfuck.github.io/main/assets/jokesbase.json')
+    .then((value)=> {
+        return value.json()
+    })
+    .then(
+        (value) =>{
+            Ljokes = value
+        })
+
+console.log(Ljokes);
+/*
 let htmlString = "";
-*/
-
-/*
-fetch('https://github.com/darkasfuck/darkasfuck.github.io/blob/main/assets/jokesbase.json')
-   .then(response => response.text())
-   .then(json => console.log(JSON.stringify(json)))
-*/
-
-const data = printJSON();
-
-async function printJSON() {
-    const response = await fetch("https://github.com/darkasfuck/darkasfuck.github.io/blob/main/assets/jokesbase.json");
-    const json = await response.json();
-    console.log(json);
-    return (json);
-}
-
-/*
+let Title="";
 for (let i=0; i<15; i++){
+    Title = Ljokes[0].title;
     htmlString += `<div class="card mb-4"> <div class="card-body"> <h2 class="card-title">כותרת בדיחה1</h2> <h5 class="ratingc"> <IMG SRC="assets/scale.png" alt="scale" width="130" height="30"> &emsp;&emsp; <IMG SRC="assets/like.png" alt="like" width="20" height="20"> ${i} <IMG SRC="assets/dislike.png" alt="dislike" width="20" height="20"> </h2> </div> </div>`
 }
 main.innerHTML = htmlString;
+
 */
