02841bc85ae03fb53f3c531f880028a59a004da5
darkasfuck <auriarad@gmail.com>
2024-03-15 18:07:48 +0200

    a

--- js/DisplayLoop.js
+++ js/DisplayLoop.js
@@ -1,5 +1,4 @@
-//const main = document.querySelector('main');
-
+const main = document.querySelector('main');
 
 let Ljokes = [];
 fetch('https://raw.githubusercontent.com/darkasfuck/darkasfuck.github.io/main/assets/jokesbase.json')
@@ -9,16 +8,19 @@ fetch('https://raw.githubusercontent.com/darkasfuck/darkasfuck.github.io/main/as
     .then(
         (value) =>{
             Ljokes = value
+            //console.log(Ljokes[0].title)
+            main.innerHTML = showstuff();
         })
 
-console.log(Ljokes);
-/*
-let htmlString = "";
-let Title="";
-for (let i=0; i<15; i++){
-    Title = Ljokes[0].title;
-    htmlString += `<div class="card mb-4"> <div class="card-body"> <h2 class="card-title">כותרת בדיחה1</h2> <h5 class="ratingc"> <IMG SRC="assets/scale.png" alt="scale" width="130" height="30"> &emsp;&emsp; <IMG SRC="assets/like.png" alt="like" width="20" height="20"> ${i} <IMG SRC="assets/dislike.png" alt="dislike" width="20" height="20"> </h2> </div> </div>`
-}
-main.innerHTML = htmlString;
 
-*/
+
+function showstuff(){
+    let htmlString = "";
+    for (let i=0; i<15; i++){
+        htmlString += `<button class="card mb-4"> <div class="card-body"> <h2 class="card-title">${Ljokes[i].title}</h2> <h5 class="ratingc"> <IMG SRC="assets/scale.png" alt="scale" width="130" height="30"> &emsp;&emsp; <IMG SRC="assets/like.png" alt="like" width="20" height="20"> ${Ljokes[i].ranking} <IMG SRC="assets/dislike.png" alt="dislike" width="20" height="20"> </h2> </div> </div>`
+    }
+    return htmlString;
+}
+
+
+
