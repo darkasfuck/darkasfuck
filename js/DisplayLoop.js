f0282e640c811f667f14cf8062dc1d3f5bed153a
darkasfuck <auriarad@gmail.com>
2024-03-27 12:59:27 +0200

    a

--- js/DisplayLoop.js
+++ js/DisplayLoop.js
@@ -17,7 +17,7 @@ fetch('https://raw.githubusercontent.com/darkasfuck/darkasfuck.github.io/main/as
 function showstuff(){
     let htmlString = "";
     for (let i=0; i<15; i++){
-        htmlString += `<a href="darkasfuck/jokesPages/${i}.html" style="color: #000000; text-decoration: none;" class="card mb-4"> <div class="card-body"> <h2 class="card-title">${Ljokes[i].title}</h2> <h5 class="ratingc"> <IMG SRC="assets/scale.png" alt="scale" width="130" height="30"> &emsp;&emsp; <IMG SRC="assets/like.png" alt="like" width="20" height="20"> ${Ljokes[i].ranking} <IMG SRC="assets/dislike.png" alt="dislike" width="20" height="20"> </h2> </div> </div>`
+        htmlString += `<a href="JokesPages/${i}.html" style="color: #000000; text-decoration: none;" class="card mb-4"> <div class="card-body"> <h2 class="card-title">${Ljokes[i].title}</h2> <h5 class="ratingc"> <IMG SRC="assets/scale.png" alt="scale" width="130" height="30"> &emsp;&emsp; <IMG SRC="assets/like.png" alt="like" width="20" height="20"> ${Ljokes[i].ranking} <IMG SRC="assets/dislike.png" alt="dislike" width="20" height="20"> </h2> </div> </div>`
     }
     return htmlString;
 }
