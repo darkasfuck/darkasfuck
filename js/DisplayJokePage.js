51def366565fdc0b47ad9d331369c90859ddd83c
darkasfuck <auriarad@gmail.com>
2024-03-27 10:42:47 +0200

    a

---
+++ js/DisplayJokePage.js
@@ -0,0 +1,23 @@
+let jid = 0;
+const main = document.querySelector('main');
+
+let Ljokes = [];
+fetch('https://raw.githubusercontent.com/darkasfuck/darkasfuck.github.io/main/assets/jokesbase.json')
+    .then((value)=> {
+        return value.json()
+    })
+    .then(
+        (value) =>{
+            Ljokes = value
+            main.innerHTML = showstuff();
+        })
+
+
+
+function showstuff(){
+    htmlString = `<h2 class="fw-normal">${Ljokes[jid].title}</h2> <p class="lead mb-0">${Ljokes[jid].body}</p>`
+    return htmlString;
+}
+
+
+
